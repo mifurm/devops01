3431329f251d46c871a71e8955e8de3fe90f9d67
mifurm <michal.furmankiewicz@hotmail.com>
2021-10-08 06:56:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "tfstatedevops"
+    key                  = "tfstatedevops.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "devopsforum" {
+  name     = "devopsforum"
+  location = "eastus2"
+}
