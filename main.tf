11def935d95b6a3ce673bd2b3c84a304c49bb97e
mifurm <michal.furmankiewicz@hotmail.com>
2021-10-08 15:30:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "devopsmf01" {
-  name     = "devopsmf01"
+resource "azurerm_resource_group" "devopsmf20" {
+  name     = "devopsmf20"
   location = "eastus2"
 }
 
@@ -24,5 +24,5 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
   address_space       = ["192.168.0.0/16"]
   location            = "eastus2"
-  resource_group_name = azurerm_resource_group.devopsmf01.name
+  resource_group_name = azurerm_resource_group.devopsmf20.name
 }
